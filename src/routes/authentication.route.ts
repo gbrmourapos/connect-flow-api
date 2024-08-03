1ebc53e3db100bd74f34e4bd5fd6119c39700039
douglasramosdev <douglas.tavaresramos@gmail.com>
2024-08-03 16:21:29 -0300

    adicionado rotas e controladores

--- src/routes/authentication.route.ts
+++ src/routes/authentication.route.ts
@@ -1,5 +1,14 @@
 import { Router } from "express";
+import { login } from "../controllers/authentication.controller";
+import { register } from "../controllers/authentication.controller";
+import { confirmTokens } from "../controllers/authentication.controller";
+import { changePassword } from "../controllers/authentication.controller";
 
 const authRoute = Router();
+
+authRoute.post("/", login);
+authRoute.post("/", register);
+authRoute.post("/", confirmTokens);
+authRoute.post("/", changePassword);
 
 export { authRoute };
